ecd742cc3d37d28939a3bf1faac8536a2bf22fba
arnaudmanaranche <arnaud@onrewind.com>
2020-10-14 17:41:18 +0200

    feat(deps): add missing deps for publishing package

--- .clean-publish.js
+++ .clean-publish.js
@@ -1,7 +1,7 @@
 // CLEAN BEFORE PUBLISH
 module.exports = {
   // Fields in package.json to remove
-  fields: ['scripts', 'husky', 'lint-staged'],
+  fields: ['scripts', 'devDependencies', 'husky', 'lint-staged'],
   // Files to remove
   files: [
     '.clean-publish.js',
